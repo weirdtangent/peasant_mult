0b43d0b5ae63925c4d7000604f4f03ec6d3279ea
Jeff Culverhouse <jeff@graystorm.com>
2020-07-14 15:16:09 +0000

    Add simple test (which needed dev-dependency)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use std::env;
 
+#[test]
+fn check_peasant_mult() {
+    use rand::Rng;
+
+    let mut rng = rand::thread_rng();
+    for _ in 0..10 {
+        let n1 = rng.gen_range(1, 10000);
+        let n2 = rng.gen_range(1, 10000);
+        assert_eq!(peasant_multiplication(n1, n2), n1 * n2);
+    }
+}
+
 /// Adapted from reading "Algorithms" by Jeff Erickson
 /// freely available on http://jeffe.cs.illinois.edu/teaching/algorithms/
 pub fn main() {
